1ad7f23d445a17ac26ba4ec5d792ccdf4963074e
vikrant1111111 <104726859+vikrant1111111@users.noreply.github.com>
2023-04-07 21:58:59 -0400

    Add files via upload

---
+++ link.js
@@ -0,0 +1,14 @@
+class Link {
+    constructor(bodyA, bodyB)
+    {
+      const firebaseConfig = {
+        apiKey: "AIzaSyDZ8CSOa3e0uWCzePCFPvL815I-t_WER5E",
+        authDomain: "proj-7f737.firebaseapp.com",
+        projectId: "proj-7f737",
+        storageBucket: "proj-7f737.appspot.com",
+        messagingSenderId: "267301161626",
+        appId: "1:267301161626:web:a326107d4080c89a72ed23",
+        measurementId: "G-QK79RY8303"
+      };
+    }
+}
